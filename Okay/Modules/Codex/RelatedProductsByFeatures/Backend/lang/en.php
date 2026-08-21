c3c1081cdd8c0d984302f2ab0fe77917e95b51ca
dabustard <dabustard@gmail.com>
2026-08-21 11:00:50 +0300

    Add related products by features module

---
+++ Okay/Modules/Codex/RelatedProductsByFeatures/Backend/lang/en.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['codex_related_products_by_features__menu'] = 'Похожие по характеристикам';
+$lang['codex_related_products_by_features__title'] = 'Автоподбор похожих товаров по характеристикам';
+$lang['codex_related_products_by_features__settings'] = 'Настройки расчёта';
+$lang['codex_related_products_by_features__category_weight'] = 'Вес совпадения основной категории';
+$lang['codex_related_products_by_features__default_limit'] = 'Количество похожих для обычных товаров';
+$lang['codex_related_products_by_features__discontinued_limit'] = 'Количество похожих для товаров со stock < 0';
+$lang['codex_related_products_by_features__batch_size'] = 'Размер AJAX-пакета товаров';
+$lang['codex_related_products_by_features__include_discontinued_related'] = 'Разрешить назначать снятые с производства товары как похожие';
+$lang['codex_related_products_by_features__only_visible_products'] = 'Обрабатывать только включённые товары';
+$lang['codex_related_products_by_features__recalculate'] = 'Переназначение похожих';
+$lang['codex_related_products_by_features__products_total'] = 'Товаров к обработке';
+$lang['codex_related_products_by_features__recalculate_hint'] = 'Пересчёт полностью очищает текущую таблицу похожих товаров и создаёт связи заново. Обработка идёт AJAX-пакетами, чтобы снизить риск таймаута.';
+$lang['codex_related_products_by_features__start'] = 'Переназначить похожие';
+$lang['codex_related_products_by_features__confirm'] = 'Текущие похожие товары будут полностью удалены и созданы заново. Продолжить?';
+$lang['codex_related_products_by_features__started'] = 'Пересчёт запущен';
+$lang['codex_related_products_by_features__progress'] = 'Обработано товаров';
+$lang['codex_related_products_by_features__inserted'] = 'создано связей';
+$lang['codex_related_products_by_features__finished'] = 'Пересчёт завершён, создано связей';
+$lang['codex_related_products_by_features__feature_weight_title'] = 'Похожие товары';
+$lang['codex_related_products_by_features__feature_weight'] = 'Вес характеристики';
+$lang['codex_related_products_by_features__feature_weight_hint'] = 'Вес участвует в автоподборе похожих товаров. Значение 0 полностью исключает характеристику из расчёта.';
